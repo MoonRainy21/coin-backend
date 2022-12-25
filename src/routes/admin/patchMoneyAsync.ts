a49ba4022ff8fbd3feb89129271364d68c7f1556
1n1tial <dan.yun0821@gmail.com>
2022-12-25 11:14:01 +0900

    add lastevents to patch money

--- src/routes/admin/patchMoneyAsync.ts
+++ src/routes/admin/patchMoneyAsync.ts
@@ -1,13 +1,15 @@
 import { Next, ParameterizedContext } from "koa";
 import { z } from "zod";
 import { UserModel } from "../../models/connection";
+import { hash } from "../../util/hash";
 import { WriteMoneyLog } from "../../util/logger";
 import { BoothEvent, State } from "../../util/types";
 
 export const patchMoneyAsync = async (ctx: ParameterizedContext<State>, next: Next) => {
     const bodyparse = z.object({
         kyoId: z.string(),
-        incresement: z.number()
+        incresement: z.number(),
+        eventName: z.optional(z.string())
     }).safeParse(ctx.request.body)
     ctx.assert(bodyparse.success, 400)
 
@@ -16,17 +18,27 @@ export const patchMoneyAsync = async (ctx: ParameterizedContext<State>, next: Ne
 
     WriteMoneyLog({
         kyoId: bodyparse.data.kyoId,
-        boothName: "ATM",
-        eventName: "ATM",
+        boothName: "Admin",
+        eventName: bodyparse.data.eventName ?? "Admin",
         time: new Date(),
         value: bodyparse.data.incresement,
         id: ""
     })
 
+    const _adminBoothEvent: BoothEvent = {
+        kyoId: bodyparse.data.kyoId,
+        eventName: "Admin",
+        boothName: bodyparse.data.eventName ?? "Admin",
+        value: bodyparse.data.incresement,
+        time: new Date(),
+        id: hash(bodyparse.data.kyoId+"ATM"+(new Date()).toISOString()),
+        isApproved: true,
+    }
+
     await user.updateOne({
         money: user.money+bodyparse.data.incresement,
         totalMoney: user.totalMoney+(bodyparse.data.incresement > 0 ? bodyparse.data.incresement : 0),
-
+        lastEvents: user.lastEvents.concat([_adminBoothEvent])
     })
     ctx.response.status = 200
 }
